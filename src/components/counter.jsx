52303c2ee5fc66b3bac33cbcda7152a8258915d5
Ginger Heef <gingerheef003@gmail.com>
2022-05-24 22:21:00 +0530

    condition with && feature

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,18 +6,18 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
+    renderTags() {
+        if ( this.state.tags.length === 0 ) return <p>There are no tags!</p>;
 
+        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>) }</ul>;
+    }
+    
     render() {
 
-        return (
-            <React.Fragment>
-                <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm'>Increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
-                </ul>
-            </React.Fragment>
-        );
+        return <div>
+            { this.state.tags.length === 0 && 'Please create a new tag' }
+            { this.renderTags() }
+        </div>;
     }
 
     getBadgeClass() {
