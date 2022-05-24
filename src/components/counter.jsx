52e816ac1e75eec229fd2483ae817b0a7c8f3d35
Ginger Heef <gingerheef003@gmail.com>
2022-05-24 16:29:31 +0530

    Using state variables

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 0
+    };
+
     render() { 
         return (
             <React.Fragment>
-                <h1>Hello World!!</h1>
+                <span>{this.state.count}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
