d3fca9d3679ac163b0c7ac624f67937e1b9dd8ee
Ginger Heef <gingerheef003@gmail.com>
2022-05-24 16:49:28 +0530

    using styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,18 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        imageURL: "https://picsum.photos/200"
+    };
+
+    styles = {
+        fontSize: 50,
+        fontWeight: 'bold'
     };
 
     render() { 
         return (
             <React.Fragment>
-                <img src={this.state.imageURL} alt="" />
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span styles={this.styles} className='badge bg-primary m-2'>{this.formatCount()}</span>
+                <button className='btn btn-secondary btn-sm'>Increment</button>
             </React.Fragment>
         );
     }
